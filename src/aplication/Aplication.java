b8a8a20912602c836a9eca596b7cfb35fe2f6757
nelsonmichels <nelsonmichels@gmail.com>
2020-05-13 10:47:30 -0300

    acrescentada msg bom dia

--- src/aplication/Aplication.java
+++ src/aplication/Aplication.java
@@ -4,7 +4,13 @@ public class Aplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("bom dia");
+		System.out.println("bom dia");
+		System.out.println("bom dia");
+		System.out.println("bom dia");
+		System.out.println("bom dia");
+		System.out.println("bom dia");
+		System.out.println("bom dia");
 	}
 
 }
